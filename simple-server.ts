1c6c0d64f2d399a1bef38296e075cf1e4586fa04
thomasgsquinaia <thomasquinaia@gmail.com>
2024-02-28 11:41:58 -0300

    [main] feat: up

---
+++ simple-server.ts
@@ -0,0 +1,37 @@
+import { ApolloServer, gql } from "apollo-server";
+
+
+const typeDefs = gql`
+type Query {
+    users: [String!]!
+}
+
+type Mutation { 
+    createUser(name: String!): String!
+}
+`
+
+const users: string[] = [];
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers: {
+        Query: {
+            users: () => {
+                return users;
+            }
+        },
+        
+        Mutation: {
+            createUser: (parent, args, ctx) => {
+                users.push(args.name)
+                return args.name
+            }
+        }
+    }
+})
+
+server.listen().then(({url}) => {
+    console.log(`App is running in port ${url}`);
+})
+
